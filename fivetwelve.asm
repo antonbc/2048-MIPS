995483031f0c625679868b8dc460b7161bd609b3
anton <125847709+antonbc@users.noreply.github.com>
2024-11-07 22:16:23 +0800

    added initial text strings

---
+++ fivetwelve.asm
@@ -0,0 +1,7 @@
+.data
+menu_msg:       .asciiz "Choose [1] New Game, [2] Start from a State, [X] Exit: "
+enter_move:     .asciiz "Enter a move (A, D, W, S): "
+win_msg:        .asciiz "Congratulations! You have reached the 512 tile!\n"
+lose_msg:       .asciiz "Game over..\n"
+invalid_input:  .asciiz "Invalid input. Try again.\n"
+enter_grid:     .asciiz "Enter a board configuration (9 numbers):\n"

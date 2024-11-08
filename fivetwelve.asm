c032e7abbf0541c031c59814d25c3eeea5189248
anton <125847709+antonbc@users.noreply.github.com>
2024-11-08 23:10:17 +0800

    add: Added some macros

--- fivetwelve.asm
+++ fivetwelve.asm
@@ -1,11 +1,28 @@
-.macro 
-    print_str(%label)
-    la $a0, %label
-    do_syscall(4)
+.macro do_syscall(%n)
+    	li $v0, %n
+    	syscall
+.end_macro
+
+.macro read_integer
+    	do_syscall(5)                 
+.end_macro
+
+.macro print_integer(%label)
+    	move $a0, %label            
+    	do_syscall(1)                 
+.end_macro
+
+.macro print_str(%label)
+    	la $a0, %label
+    	do_syscall(4)
+.end_macro
+
+.macro exit
+   	    do_syscall(10)                
 .end_macro
 
 .data
-menu_msg:       .asciiz "Choose [1] New Game, [2] Start from a State, [X] Exit: "
+menu_msg:       .asciiz "Choose [1] New Game \n [2] Start from a State \n"
 enter_move:     .asciiz "Enter a move (A, D, W, S): "
 win_msg:        .asciiz "Congratulations! You have reached the 512 tile!\n"
 lose_msg:       .asciiz "Game over..\n"
@@ -15,3 +32,5 @@ enter_grid:     .asciiz "Enter a board configuration (9 numbers):\n"
 
 .text
 main:
+    
+    exit
